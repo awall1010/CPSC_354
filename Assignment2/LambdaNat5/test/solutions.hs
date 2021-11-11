e9c925da77011bdb9ca63ee6b11d36afad8b6166
awall1010 <awall@chapman.edu>
2021-11-10 22:10:40 -0800

    converted rest to haskell and ran test cases

--- Assignment2/LambdaNat5/test/solutions.hs
+++ Assignment2/LambdaNat5/test/solutions.hs
@@ -20,13 +20,25 @@ prodList (x:xs) = x * prodList xs
 plus_two :: Int -> Int
 plus_two x = x + 2
 
+mapList :: (a -> b) -> [a] -> [b]
+mapList f [] = []
+mapList f (x:xs) = f x : mapList f xs
 
+
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys) | x <= y = x:y:ys
+                | otherwise = y:(insert x ys)
+
+sort :: Ord a => [a] -> [a]
+sort [] = []
+sort (x:xs) = insert x (sort xs)
 
 
 main = do
-  --print $ (member 1 [2,3,1]);
-  --print $ (removeList 2 [1,2,3,1,2,3]);
-  --print $ (sumList [1,2,3,4]);
-  --print $ (prodList [1,2,3,4]);
-  --print $ (mapList plus_two [1,2,3,4]);
-  --print $ (sort [1,2,3,1,2,3]);
+  print $ (member 1 [2,3,1]);
+  print $ (removeList 2 [1,2,3,1,2,3]);
+  print $ (sumList [1,2,3,4]);
+  print $ (prodList [1,2,3,4]);
+  print $ (mapList plus_two [1,2,3,4]);
+  print $ (sort [1,2,3,1,2,3]);
