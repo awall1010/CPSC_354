c3812406fff5d5e10b8dd9178fb8b47d2b204d6f
awall1010 <awall@chapman.edu>
2021-11-10 21:40:07 -0800

    in haskell added member, rList, sList, pList and plus2

---
+++ Assignment2/LambdaNat5/test/solutions.hs
@@ -0,0 +1,32 @@
+member :: (Eq a) => a -> [a] -> Bool
+member x [] = False
+member x (y:ys) | x==y = True
+                | otherwise = member x ys
+
+removeList :: Int -> [Int] -> [Int]
+removeList _ [] = []
+removeList x (y:ys) | x == y = ys
+                | otherwise = y : remove x ys
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
+
+prodList :: [Int] -> Int
+prodList [] = 1
+prodList (x:xs) = x * prodList xs
+
+
+plus_two :: Int -> Int
+plus_two x = x + 2
+
+
+
+
+main = do
+  --print $ (member 1 [2,3,1]);
+  --print $ (removeList 2 [1,2,3,1,2,3]);
+  --print $ (sumList [1,2,3,4]);
+  --print $ (prodList [1,2,3,4]);
+  --print $ (mapList plus_two [1,2,3,4]);
+  --print $ (sort [1,2,3,1,2,3]);
